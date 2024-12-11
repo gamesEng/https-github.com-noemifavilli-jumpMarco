1d5e306076f0be0fb14adb2ee7eda6b1f4f9cd22
josephm89 <joseph.meszaros89@yahoo.co.uk>
2024-12-11 11:34:50 +0000

    add comments, remove the first move

--- jump_marco/components/cmp_enemy_ai.cpp
+++ jump_marco/components/cmp_enemy_ai.cpp
@@ -4,17 +4,22 @@ using namespace std;
 using namespace sf;
 
 void EnemyAIComponent::update(double dt) {
-  auto mov = _direction * (float)(dt * _speed);
-  mov.x += _direction.x * 16.f;
-  if (!validMove(_parent->getPosition() + mov)) {
-    _direction *= -1.f;
-  }
+    // Calculate movement
+    auto mov = _direction * (float)(dt * _speed);
 
-  move(_direction * (float)(dt * _speed));
-  ActorMovementComponent::update(dt);
+    // Check if the enemy can move; reverse direction if not
+    if (!validMove(_parent->getPosition() + mov)) {
+        _direction *= -1.f; // Reverse direction
+    }
+
+    // Move the enemy
+    move(_direction * (float)(dt * _speed));
+
+    // Call parent update for any additional logic
+    ActorMovementComponent::update(dt);
 }
 
 EnemyAIComponent::EnemyAIComponent(Entity* p) : ActorMovementComponent(p) {
-  _direction = Vector2f(1.0f, 0);
-  _speed = 100.0f;
+    _direction = Vector2f(1.0f, 0); // Start moving right
+    _speed = 50.0f;                // Movement speed
 }
