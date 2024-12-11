720d4e186d7b345551c5d7d1c7bb65ced946c587
josephm89 <joseph.meszaros89@yahoo.co.uk>
2024-12-11 17:58:47 +0000

    instead of deleting player entity it logs out aouch

--- jump_marco/components/cmp_hurt_player.cpp
+++ jump_marco/components/cmp_hurt_player.cpp
@@ -1,17 +1,22 @@
 #include "cmp_hurt_player.h"
 #include <engine.h>
 
+
 using namespace std;
 using namespace sf;
 
 void HurtComponent::update(double dt) {
-  if (auto pl = _player.lock()) {
-    if (length(pl->getPosition() - _parent->getPosition()) < 25.0) {
-      pl->setForDelete();
-      _parent->setForDelete();
+    if (auto pl = _player.lock()) { // Check if player is valid
+        if (length(pl->getPosition() - _parent->getPosition()) < 25.0) {
+            // Instead of deleting, just log "Ouch!"
+            std::cout << "Ouch!" << std::endl;
+        }
     }
-  }
 }
 
 HurtComponent::HurtComponent(Entity* p)
-    : Component(p), _player(_parent->scene->ents.find("player")[0]) {}
+    : Component(p), _player(_parent->scene->ents.find("player")[0]) {
+    if (!p) {
+        std::cerr << "Error: Could not find entity with tag 'player'!" << std::endl;
+    }
+}
